fd2599d15085b792203e3bd73c3f2e0785ccf418
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2023-10-10 15:25:56 +0300

    Separated products related code to different file.

--- customer.go
+++ customer.go
@@ -3,7 +3,6 @@ package nettikauppasimulaattori
 // TODO: Break into separate files:
 // customers.go, orders.go, products.go, run.go etc.
 
-
 import (
 	"errors"
 	"math"
@@ -19,15 +18,6 @@ type Customer struct {
     price_point int
     base_purchase_probability float64
 }
-
-type Product struct {
-    id int
-    name string
-    wholesale_price int
-    price int
-    vat float64
-}
-
 
 var Customers = []Customer{
     {10,  "Erkki",    "Nyrhinen"   , 6,  150, 0.15 },
@@ -64,31 +54,6 @@ var Customers = []Customer{
     {217, "Sabrina",  "Bqain"      , 23, 150, 0.10 },  
     {218, "Tara",     "Junker"     , 23, 150, 0.10 },
 }
-
-
-var Products = []Product{   
-    {1001, "Pirkka olut 24-pak.", 10, 25, 0.24},
-    {1002, "Pirkka olut 6-pak.", 3, 8, 0.24},
-    {2001, "Raspberry Pi 4 4GB", 40, 80, 0.24},
-    {2002, "Raspberry Pi 4 8GB", 50, 100, 0.24},
-    {2003, "VHS-kasetteja 10-pak", 5, 8, 0.24},
-    {2004, "C-kasetteja 10-pak", 3, 8, 0.24},
-    {2005, "LCD-televisio", 150, 300, 0.24},
-    {2006, "Iso LCD-televisio", 200, 400, 0.24},
-
-    {3001, "Ruisleipä", 1, 3, 0.14},
-    {3002, "Rasvaton Maito 1L", 1, 2, 0.14},
-    {3003, "Kevytmaito 1L", 1, 2, 0.14},
-    {3004, "Täysmaito 1L", 1, 2, 0.14},
-    {3005, "Kauraleipä", 1, 3, 0.14},
-    {3006, "Mysliä 1kg", 1, 4, 0.14},
-    {3007, "Perunoita 1kg", 1, 2, 0.14},
-
-    {4000, "Silmarillion, J.R.R Tolkien", 10, 25, 0.10},
-    {4001, "Tabu, Timo Mukka", 5, 15, 0.10},
-    {4002, "Robottien aamunkoitto, Isaac Asimov", 10, 15, 0.10},
-    {4003, "Holmenkollen, Matti Hagelberg", 15, 30, 0.10},
-}   
 
 func calc_probability(x int, base_probability float64, target int, spread int) float64 {
     // Calculate probability which drops as we get further away from 'target'.
---
+++ products.go
@@ -0,0 +1,35 @@
+package nettikauppasimulaattori
+
+// TODO: Not actually necessary to have price and other info on this side.
+
+type Product struct {
+    id int
+    name string
+    wholesale_price int
+    price int
+    vat float64
+}
+
+var Products = []Product{   
+    {1001, "Pirkka olut 24-pak.", 10, 25, 0.24},
+    {1002, "Pirkka olut 6-pak.", 3, 8, 0.24},
+    {2001, "Raspberry Pi 4 4GB", 40, 80, 0.24},
+    {2002, "Raspberry Pi 4 8GB", 50, 100, 0.24},
+    {2003, "VHS-kasetteja 10-pak", 5, 8, 0.24},
+    {2004, "C-kasetteja 10-pak", 3, 8, 0.24},
+    {2005, "LCD-televisio", 150, 300, 0.24},
+    {2006, "Iso LCD-televisio", 200, 400, 0.24},
+
+    {3001, "Ruisleipä", 1, 3, 0.14},
+    {3002, "Rasvaton Maito 1L", 1, 2, 0.14},
+    {3003, "Kevytmaito 1L", 1, 2, 0.14},
+    {3004, "Täysmaito 1L", 1, 2, 0.14},
+    {3005, "Kauraleipä", 1, 3, 0.14},
+    {3006, "Mysliä 1kg", 1, 4, 0.14},
+    {3007, "Perunoita 1kg", 1, 2, 0.14},
+
+    {4000, "Silmarillion, J.R.R Tolkien", 10, 25, 0.10},
+    {4001, "Tabu, Timo Mukka", 5, 15, 0.10},
+    {4002, "Robottien aamunkoitto, Isaac Asimov", 10, 15, 0.10},
+    {4003, "Holmenkollen, Matti Hagelberg", 15, 30, 0.10},
+}   
