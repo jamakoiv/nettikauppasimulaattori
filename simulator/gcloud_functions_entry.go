386d7a4fadfec03324f59dab34565c221424d385
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2023-12-15 20:15:26 +0200

    Moved running customers and workers to separate functions.

--- simulator/gcloud_functions_entry.go
+++ simulator/gcloud_functions_entry.go
@@ -23,19 +23,17 @@ func init() {
 	functions.CloudEvent("Run", Run)
 }
 
-func Run(ctx context.Context, ev event.Event) error {
-    // TODO: Project_id hardcoded in two different places :(
-    project_id := "nettikauppasimulaattori"
-
-    slog.Info(fmt.Sprintf("Program started at %v", time.Now()))
-
-    client, err := bigquery.NewClient(ctx, project_id)
-    if err != nil { 
-        slog.Error("Error creating BigQuery-client.") 
-        return err
+func RunWorkers(ctx context.Context, client* bigquery.Client) {
+    for _, worker := range Workers {
+        slog.Info(fmt.Sprintf("Checking worker %d.", worker.id))
+        err := worker.Work(ctx, client)
+        if err != nil {
+            slog.Error(fmt.Sprint(err))
+        }
     }
-    defer client.Close()
+}
 
+func RunCustomers(ctx context.Context, client* bigquery.Client) {
     orders_in_this_run := false
     for _, customer := range Customers {
         order, err := customer.Shop(Products)
@@ -53,11 +51,23 @@ func Run(ctx context.Context, ev event.Event) error {
     if !orders_in_this_run {
         slog.Info("No orders placed this time.")
     }
+}
 
-    for _, worker := range Workers {
-        slog.Info(fmt.Sprintf("Checking worker %d.", worker.id))
-        worker.Work(ctx, client)
+func Run(ctx context.Context, ev event.Event) error {
+    // TODO: Project_id hardcoded in two different places :(
+    project_id := "nettikauppasimulaattori"
+
+    slog.Info(fmt.Sprintf("Program started at %v", time.Now()))
+
+    client, err := bigquery.NewClient(ctx, project_id)
+    if err != nil { 
+        slog.Error("Error creating BigQuery-client.") 
+        return err
     }
+    defer client.Close()
+
+    RunCustomers(ctx, client)
+    // RunWorkers(ctx, client)
 
     return nil
 }
