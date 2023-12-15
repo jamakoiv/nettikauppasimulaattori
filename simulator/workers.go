dcc88988c173a32725e6052defae236f1acc9637
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2023-12-15 10:36:06 +0200

    Added workers.

--- simulator/workers.go
+++ simulator/workers.go
@@ -16,13 +16,91 @@ type Worker struct {
     id int
     first_name string
     last_name string
-    work_days []int
+    work_days []time.Weekday
     work_hours []int
     orders_per_hour int
     salary_per_hour int
 }
 
-var Workers = []Worker{}
+var Workers = []Worker{
+    {
+    111, "Vesa", "Sisättö", 
+    []time.Weekday{time.Monday, 
+        time.Tuesday, 
+        time.Wednesday, 
+        time.Thursday,
+        time.Friday},
+    []int{8, 9, 10, 11, 13, 14, 15},
+    4, 15},
+
+    {222, "Seppo", "Lahnakainen", 
+    []time.Weekday{time.Monday, 
+        time.Tuesday, 
+        time.Wednesday, 
+        time.Thursday,
+        time.Friday},
+    []int{8, 9, 10, 11, 13, 14, 15},
+    4, 15},
+
+    {333, "Janne", "Virtanen", 
+    []time.Weekday{time.Monday, 
+        time.Tuesday, 
+        time.Wednesday, 
+        time.Thursday},
+    []int{12, 13, 14, 15, 16},
+    4, 15},
+
+    {444, "Erkki", "Kolehmainen", 
+    []time.Weekday{time.Monday, 
+        time.Tuesday, 
+        time.Wednesday, 
+        time.Thursday,
+        time.Friday},
+    []int{9, 10, 11, 12, 13, 14, 15, 16},
+    3, 15},
+
+    {555, "Laura", "Kolehmainen", 
+    []time.Weekday{time.Monday, 
+        time.Tuesday, 
+        time.Wednesday, 
+        time.Thursday,
+        time.Friday},
+    []int{9, 10, 11, 12, 13, 14, 15, 16},
+    3, 15},
+
+    {555, "Laura", "Kolehmainen", 
+    []time.Weekday{time.Monday, 
+        time.Tuesday, 
+        time.Wednesday, 
+        time.Thursday,
+        time.Friday},
+    []int{9, 10, 11, 12, 13, 14, 15, 16},
+    3, 15},
+
+    {666, "Sanna", "Sörppö",
+    []time.Weekday{time.Tuesday, 
+        time.Wednesday, 
+        time.Thursday,
+        time.Friday,
+        time.Saturday},
+    []int{12, 13, 14, 15, 16, 17, 18, 19, 20},
+    4, 15},
+
+    {777, "Ville", "Korhonen",
+    []time.Weekday{time.Monday, 
+        time.Tuesday, 
+        time.Wednesday, 
+        time.Thursday,
+        time.Friday},
+    []int{12, 13, 14, 15, 16, 17, 18, 19, 20},
+    3, 15},
+
+    {888, "Kiire", "Apulainen",
+    []time.Weekday{time.Saturday,
+        time.Sunday},
+    []int{12, 13, 14, 15, 16, 17, 18},
+    3, 15},
+}
 
 func (w *Worker) GetDailySalary() int {
     return len(w.work_hours) * w.salary_per_hour 
