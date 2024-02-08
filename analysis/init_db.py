a29273422030063be71d2327a8faba6212c57d14
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2024-02-08 15:49:25 +0200

    Added function for creating customers and data file containing median wages per postal code.

--- analysis/init_db.py
+++ analysis/init_db.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import random
+from dataclasses import dataclass
+
 
 delivery_types = pd.DataFrame(
     [[0, "SHIP TO CUSTOMER"], [1, "COLLECT FROM STORE"]],
@@ -21,8 +24,62 @@ orders = pd.DataFrame({
     'last_modified': pd.Series(dtype='datetime64[ns]')
 })
 
+
+
 order_items = pd.DataFrame({
     'order_id': pd.Series(dtype='int'),
     'product_id': pd.Series(dtype='int')
 })
 
+customers = pd.DataFrame({
+    'id': pd.Series(dtype='int'),
+    'name': pd.Series(dtype='str'),
+    'area': pd.Series(dtype='int'),
+    'most_active': pd.Series(dtype='int'),
+    'purchase_chance': pd.Series(dtype='float'),
+    'max_budget': pd.Series(dtype='float'),
+    'product_categories': pd.Series(dtype='str')
+})
+
+
+@dataclass
+class Customer:
+    id: int
+    name: str
+    area: int
+    most_active: int
+    max_budget: float
+    purchase_chance: float
+    product_categories: list[int]
+
+    ...
+
+
+
+def create_customers(N: int, 
+                     income_per_area: pd.DataFrame,
+                     destination_table: pd.DataFrame) -> pd.DataFrame: 
+
+    weights = np.random.random(size=len(income_per_area))
+    areas = random.choices(income_per_area['code'], weights, k=N)
+
+    # 
+    income_floor = 8000
+    income_coeff = 0.02 
+
+    max_purchase_chance = 0.10
+
+    for i, area in enumerate(areas):
+        active = np.random.normal(15.0, 6) % 24  # modulo forces value to 0-24 range.
+
+        mask = income_per_area['code'] == area
+        budget = (income_per_area[mask]['income'].values[0] - income_floor)
+        budget *= np.random.random() * income_coeff
+
+        purchase_chance = np.random.random() * max_purchase_chance
+
+        destination_table.loc[i] = [i, '', area, 
+                                    active, purchase_chance, 
+                                    budget, '{1;2;3;4}']
+
+    return destination_table
