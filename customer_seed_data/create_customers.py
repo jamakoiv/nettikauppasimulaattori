0ee7438ce3fb6ce185568f22a0fbbff1c5b2a904
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2024-02-20 16:09:50 +0100

    Basic functionality coming along...

--- customer_seed_data/create_customers.py
+++ customer_seed_data/create_customers.py
@@ -1,6 +1,7 @@
-import pandas as pd
 import copy
 import random
+import pandas as pd
+import numpy as np
 
 from import_seed import import_all
 
@@ -42,26 +43,59 @@ def create_customers(N: int,
     income_weights = income[income_labels].div(income['over_18'], axis=0)
 
     age_drop_labels = ['code', 'area', 'male', 'female', 'avg', 'pop']
+    age_labels = age.drop(age_drop_labels, axis=1).columns
     age_weights = age.drop(age_drop_labels, axis=1).div(age['pop'], axis=0)
 
     gender_labels = ['male', 'female']
     gender_weights = age[gender_labels].div(age['pop'], axis=0)
 
-    breakpoint()
+    occupation_labels = ['employed', 'unemployed', 'students', 'retired', 'other']
+    occupation_weights = occupation[occupation_labels].div(occupation['pop'], axis=0)
+
+
     codes = random.choices(income.index.astype('int'), 
                            weights=code_weights.values.astype('float64'), 
                            k=N)
-    income_brackets = [random.choices(income_labels, 
-                            income_weights.loc[code]) for code in codes]
-    ages = [random.choices() for code in codes]
 
-    breakpoint()
+    # TODO: Make separate function for picking all the customer parameters.
+    # This way we can better control the results (don't want 18 years 
+    # old retirees), and easier to use in multiprocessing.map if we need
+    # to create large amount of customers.
+
+    # NOTE: random.choices always returns list even when retrieving single value.
+    # Hence the [0].
+    income_brackets = [random.choices(income_labels, income_weights.loc[code])[0]
+                       for code in codes]
+
+    ages = [get_age(random.choices(age_labels, age_weights.loc[code])[0])
+            for code in codes]
+
+    genders = [random.choices(gender_labels, gender_weights.loc[code])[0]
+               for code in codes]
+
+    occupations = [random.choices(occupation_labels, occupation_weights.loc[code])[0]
+                   for code in codes]
+
+    res = pd.DataFrame( {
+        'code': pd.Series(codes, dtype='int'),
+        'age': pd.Series(ages, dtype='int'),
+        'gender': pd.Series(genders, dtype='str'),
+        'occupation': pd.Series(occupations, dtype='str'),
+        'income_bracket': pd.Series(income_brackets, dtype='str')
+    })
+
+    return res
 
 
-    return 
+def get_age(age: str) -> int:
+    """Get random age from age-label in the form of '20_25'."""
+
+    begin, end = [int(val) for val in age.split(sep='_')]
+    res = np.random.randint(begin, end+1)
+    return res
 
 if __name__ == "__main__":
     income, age, education, occupation = import_all()
 
-    _ = create_customers(100, income, age, education, occupation)
+    customers = create_customers(100, income, age, education, occupation)
     
