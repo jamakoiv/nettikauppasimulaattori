9d4f31a115ab86539353dddc8853a6c04fea643b
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2024-09-14 14:27:44 +0300

    Trying to get mongosh script working...

---
+++ misc/testi.js
@@ -0,0 +1,2 @@
+db = connect('mongodb://jamakoiv:Tuhtimau123@cosmos-mongo-testi.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000wtimeoutMS=0');
+db.reviews.find();
--- misc/verkkokauppa_json_extractor.sh
+++ misc/verkkokauppa_json_extractor.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/bash
 
-USER="jamakoiv"
-CONN="mongodb+srv://$USER@cosmos-mongo-testi.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+USER=jamakoiv
+PASS=$(cat passwd)
+CONN="mongodb+srv://$USER:$PASS@cosmos-mongo-testi.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000wtimeoutMS=0"
+DATABASE=reviews
 
-JSON=$(curl -s https://www.verkkokauppa.com/fi/product/893623/FWD-HP-EliteBook-840-G5-14-kaytetty-kannettava-tietokone-B-l | grep reviewText)
+# Extract the product JSON-file from the website.
+# JSON=$(curl -s https://www.verkkokauppa.com/fi/product/893623/FWD-HP-EliteBook-840-G5-14-kaytetty-kannettava-tietokone-B-l | grep reviewText)
+JSON=UGUU
 
-echo $CONN
-echo $JSON
+# NOTE: Trying to send the data using --eval=<command> usually fails due to
+# the JSON string exceeding command argument limit. That's why we make
+# a separate script file instead.
+
+# Create a mongosh-script for sending the JSON to the database.
+echo "db = connect('$CONN');" >testi.js
+echo "db.$DATABASE.find();" >>testi.js
+
+mongosh --nodb --file=testi.js $CONN
+
+# echo $CONN
+# echo $JSON
 
 # db = connect($CONN)
