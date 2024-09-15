1719e44f8591d73672d28d71d162420a02aeee22
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2024-09-15 13:29:34 +0300

    Now saves the found json into a file as well.

--- misc/verkkokauppa_json_extractor.sh
+++ misc/verkkokauppa_json_extractor.sh
@@ -9,10 +9,11 @@ URL=$1
 SIGNATURE=reviewText # Some grep-line from the JSON which we want to capture.
 
 # Extract the product JSON-file from the website.
-JSON=$(curl -s $URL | grep $SIGNATURE)
+JSON_FILE=$(echo $URL | tr -d [/])
+JSON=$(curl -s $URL | grep $SIGNATURE | tee $JSON_FILE.json)
 RES=$?
 if [ $RES -eq 1 ]; then
-  echo Error number $RES: probably grep did not find the SIGNATURE string it was looking for.
+  echo Error number $RES: probably grep did not find the SIGNATURE string it was looking for, or the automatically generated JSON_FILE filename contains illegal characters.
   exit 10
 elif [ $RES -ne 0 ]; then
   echo Error number $RES: probably retrieving the website '$URL' failed.
@@ -24,10 +25,10 @@ fi
 # a separate script file instead.
 
 # Create a mongosh-script for sending the JSON to the database.
-echo "db = db.getSiblingDB('$DATABASE');" >testi.js
-echo "db.$DATABASE.insertOne($JSON);" >>testi.js
-echo "printjson(db.$DATABASE.find());" >>testi.js
-
-mongosh --file=testi.js $CONN
+# echo "db = db.getSiblingDB('$DATABASE');" >testi.js
+# echo "db.$DATABASE.insertOne($JSON);" >>testi.js
+# echo "printjson(db.$DATABASE.find());" >>testi.js
+# 
+# mongosh --file=testi.js $CONN
 
 # rm testi.js
