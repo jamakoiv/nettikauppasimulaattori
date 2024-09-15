a92f84951d190a13fff8133539723ae70f164dd2
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2024-09-15 14:06:33 +0300

    Added some command line arguments.

--- misc/verkkokauppa_json_extractor.sh
+++ misc/verkkokauppa_json_extractor.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/bash
 
+# TODO: Make these command line arguments as well.
 USER=jamakoiv
 PASS=$(cat passwd)
 CONN="mongodb+srv://$USER:$PASS@cosmos-mongo-testi.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000wtimeoutMS=0"
 DATABASE=reviews
+SIGNATURE=reviewText
 
-URL=$1
-SIGNATURE=reviewText # Some grep-line from the JSON which we want to capture.
+# Parse input arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --save-json)
+      SAVE_JSON=true
+      shift # past argument
+      ;;
+    --upload)
+      UPLOAD=true
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      URL=$1 # save positional arg
+      break
+      ;;
+  esac
+done
+
+if [ "$SAVE_JSON" = true ]; then
+  JSON_FILE=$(echo $URL | tr -d [/]).json
+else 
+  JSON_FILE=/dev/null
+fi
 
 # Extract the product JSON-file from the website.
-JSON_FILE=$(echo $URL | tr -d [/])
-JSON=$(curl -s $URL | grep $SIGNATURE | tee $JSON_FILE.json)
+JSON=$(curl -s $URL | grep $SIGNATURE | tee $JSON_FILE)
 RES=$?
 if [ $RES -eq 1 ]; then
   echo Error number $RES: probably grep did not find the SIGNATURE string it was looking for, or the automatically generated JSON_FILE filename contains illegal characters.
@@ -25,10 +51,12 @@ fi
 # a separate script file instead.
 
 # Create a mongosh-script for sending the JSON to the database.
-# echo "db = db.getSiblingDB('$DATABASE');" >testi.js
-# echo "db.$DATABASE.insertOne($JSON);" >>testi.js
-# echo "printjson(db.$DATABASE.find());" >>testi.js
-# 
-# mongosh --file=testi.js $CONN
+if [ "$UPLOAD" = true ]; then
+  echo "db = db.getSiblingDB('$DATABASE');" >upload.js
+  echo "db.$DATABASE.insertOne($JSON);" >>upload.js
+  echo "printjson(db.$DATABASE.find());" >>upload.js
 
-# rm testi.js
+  mongosh --file=upload.js $CONN
+  rm upload.js
+fi
+
