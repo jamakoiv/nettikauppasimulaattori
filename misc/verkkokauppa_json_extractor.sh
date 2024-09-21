d05447634e831ab2cdf11008985d3f4ea172a792
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2024-09-21 13:29:22 +0300

    optimized some stuff.

--- misc/verkkokauppa_json_extractor.sh
+++ misc/verkkokauppa_json_extractor.sh
@@ -7,6 +7,7 @@ CONN="mongodb+srv://$USER:$PASS@cosmos-mongo-testi.mongocluster.cosmos.azure.com
 DATABASE=verkkokauppa
 COLLECTION=items
 SIGNATURE=reviewText # we try to find the JSON line grepping for this signature.
+URLS=()
 
 # Parse input arguments
 while [[ $# -gt 0 ]]; do
@@ -24,27 +25,47 @@ while [[ $# -gt 0 ]]; do
     exit 1
     ;;
   *)
-    URL=$1 # save positional arg
-    break
+    URLS+=("$1") # save positional arg
+    shift
     ;;
   esac
 done
+echo ${URLS[@]}
 
-if [ "$SAVE_JSON" = true ]; then
-  JSON_FILE=$(echo $URL | tr -d [/]).json
-else
-  JSON_FILE=/dev/null
-fi
+# TODO: Horrible way to construct the upload.js file...
+echo "db = db.getSiblingDB('$DATABASE');" >upload.js
+echo "db.$COLLECTION.insertMany([$JSON" >>upload.js
 
-# Extract the product JSON-file from the website.
-JSON=$(curl -s $URL | grep $SIGNATURE | tee $JSON_FILE)
-RES=$?
-if [ $RES -eq 1 ]; then
-  echo Error number $RES: probably grep did not find the SIGNATURE string it was looking for, or the automatically generated JSON_FILE filename contains illegal characters.
-  exit 10
-elif [ $RES -ne 0 ]; then
-  echo Error number $RES: probably retrieving the website '$URL' failed.
-  exit 11
+for URL in "${URLS[@]}"; do
+  URL=$(echo $URL | cut -d# -f1) # Stript hash params from the URL.
+  echo $URL
+
+  if [ "$SAVE_JSON" = true ]; then
+    JSON_FILE=$(echo $URL | tr -d [/]).json
+  else
+    JSON_FILE=/dev/null
+  fi
+
+  # Extract the product JSON-file from the website.
+  JSON=$(curl -s $URL | grep $SIGNATURE | tee $JSON_FILE)
+  RES=$?
+  if [ $RES -eq 1 ]; then
+    echo Error number $RES: probably grep did not find the SIGNATURE string it was looking for, or the automatically generated JSON_FILE filename contains illegal characters.
+    exit 10
+  elif [ $RES -ne 0 ]; then
+    echo Error number $RES: probably retrieving the website '$URL' failed.
+    exit 11
+  fi
+  PRODUCT_ID=$
+
+  echo "$JSON," >>upload.js
+
+done
+echo "]);" >>upload.js
+
+if [ "$UPLOAD" = true ]; then
+  mongosh --file=upload.js $CONN
+  rm upload.js
 fi
 
 # NOTE: Trying to send the data using --eval=<command> usually fails due to
@@ -52,11 +73,3 @@ fi
 # a separate script file instead.
 
 # Create a mongosh-script for sending the JSON to the database.
-if [ "$UPLOAD" = true ]; then
-  echo "db = db.getSiblingDB('$DATABASE');" >upload.js
-  echo "db.$COLLECTION.insertOne($JSON);" >>upload.js
-  echo "printjson(db.$DATABASE.find());" >>upload.js
-
-  mongosh --file=upload.js $CONN
-  rm upload.js
-fi
